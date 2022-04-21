5a948cd16471f556e015e16f0c8859007c69cedd
PickleRick <bmkersey@gmail.com>
2022-04-21 13:25:10 -0400

    added pool of password characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charCount = 0
 
 function generatePassword(){
-  var charCount = window.prompt("Password may contain 8-128 characters. Enter desired number of characters.")
+  if (charCount < 7 || charCount > 128){
+    var charCount = window.prompt("Password may contain 8-128 characters. Enter desired number of characters.")
+  }
   var lowerCase = window.confirm("Do you want lower case characters?");
   var upperCase = window.confirm("Do you want upper case characters?");
   var numberals = window.confirm("Do you want numbers?");
@@ -10,25 +13,28 @@ function generatePassword(){
 
   charCount = parseInt(charCount)
 
-  var passwordType ="Your password will contain: " + charCount + ", "
+  var passwordType ="Your password will contain: " + charCount + ", ";
+  var passwordPool = "";
 
-  
+
   if (lowerCase){
-    var passwordType = passwordType + "lower case, "
-  }
+    var passwordType = passwordType + "lower case, ";
+    var passwordPool ="abcdefghijklmnopqrstuvwxyz";
+  };
   if (upperCase){
-    var passwordType = passwordType + "upper case, "
-  }
+    var passwordType = passwordType + "upper case, ";
+    var passwordPool = passwordPool + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  };
   if (numberals){
-    var passwordType = passwordType + "numbers, "
-  }
+    var passwordType = passwordType + "numbers, ";
+    var passwordPool = passwordPool + "1234567890";
+  };
   if (specialChars){
-    var passwordType = passwordType + "special characters."
-  }
+    var passwordType = passwordType + "special characters.";
+    var passwordPool = passwordPool + "!@#$%^&*()_+-="
+  };
 
-  window.alert(passwordType)
-
-  
+  window.alert(passwordType)  
 
 }
 
